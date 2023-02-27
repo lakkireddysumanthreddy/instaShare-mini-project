184368d8946569dbc077eaad84f279f24c5fbb92
sumanth <lakkireddysumanthreddy.95025@gmail.com>
2023-02-27 21:22:28 +0000

    message

---
+++ src/components/ProfileCard/index.js
@@ -0,0 +1,72 @@
+import {BsGrid3X3} from 'react-icons/bs'
+
+import UserStories from '../UserStories'
+import UserPostsList from '../UserPostsList'
+
+import './index.css'
+
+const ProfileCard = props => {
+  const {data, my} = props
+
+  const {profile} = data
+  const {
+    userId,
+    userName,
+    profilePic,
+    followersCount,
+    followingCount,
+    userBio,
+    posts,
+    stories,
+    postsCount,
+  } = profile
+
+  return (
+    <li className="profile-details-new-container">
+      <li className="Profile-card">
+        <div className="image-user-details">
+          <div className="profile-pic-container">
+            <img
+              src={profilePic}
+              alt={`${my} profile`}
+              className="Profile-pic"
+            />
+          </div>
+          <div className="user-detail-container">
+            <h1 className="profile-head">{userName}</h1>
+            <div className="count">
+              <p className="count-follower">
+                <span className="highlight">{postsCount}</span>Posts
+              </p>
+              <p className="count-follower">
+                <span className="highlight">{followersCount} </span> Followers
+              </p>
+              <p className="count-follower">
+                <span className="highlight">{followingCount} </span> Following
+              </p>
+            </div>
+            <div className="bio-container">
+              <p className="bio-name">{userId}</p>
+              <p className="bio">{userBio}</p>
+            </div>
+          </div>
+        </div>
+        <ul className="user-stories">
+          {stories.map(each => (
+            <UserStories key={each.id} item={each} my={my} />
+          ))}
+        </ul>
+        <hr className="line" />
+        <div className="posts-container-user">
+          <div className="head-container">
+            <BsGrid3X3 className="post-logo" />
+            <h1 className="post-head">Posts</h1>
+          </div>
+          <UserPostsList posts={posts} my={my} />
+        </div>
+      </li>
+    </li>
+  )
+}
+
+export default ProfileCard
---
+++ src/components/UserPostsList/index.js
@@ -0,0 +1,36 @@
+import {BiCamera} from 'react-icons/bi'
+import './index.css'
+
+const UserPostsList = props => {
+  const {posts, my} = props
+
+  const postsView = () => (
+    <ul className="user-posts-container">
+      {posts.map(each => (
+        <li key={each.id}>
+          <div>
+            <img src={each.image} alt={`${my} post`} className="post-image" />
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+
+  const noPostView = () => (
+    <div className="no-post-container">
+      <BiCamera className="no-post-image" />
+      <h1 className="no-post-head">No Posts Yet</h1>
+    </div>
+  )
+
+  const correctView = () => {
+    if (posts.length === 0) {
+      return noPostView()
+    }
+    return postsView()
+  }
+
+  return correctView()
+}
+
+export default UserPostsList
